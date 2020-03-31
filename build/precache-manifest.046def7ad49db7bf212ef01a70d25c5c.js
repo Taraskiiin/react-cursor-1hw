2aaf80ff0af0d2ea62d69513283b0f3a9c831f1f
taraskiiin <taraskravets95@gmail.com>
2020-03-31 10:09:51 +0300

    reactnahoiotputi

---
+++ build/precache-manifest.046def7ad49db7bf212ef01a70d25c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdc1599076b55575c3de24477d8a815",
+    "url": "/Taraskiiin/react-cursor-1hw.git/index.html"
+  },
+  {
+    "revision": "013d8e4f12e9a4dbad25",
+    "url": "/Taraskiiin/react-cursor-1hw.git/static/css/main.9a102ae6.chunk.css"
+  },
+  {
+    "revision": "02ff096cc71da2953c87",
+    "url": "/Taraskiiin/react-cursor-1hw.git/static/js/2.e0113e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Taraskiiin/react-cursor-1hw.git/static/js/2.e0113e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013d8e4f12e9a4dbad25",
+    "url": "/Taraskiiin/react-cursor-1hw.git/static/js/main.15ab906f.chunk.js"
+  },
+  {
+    "revision": "286a79b5ddcb7bfd04b2",
+    "url": "/Taraskiiin/react-cursor-1hw.git/static/js/runtime-main.8eaf800e.js"
+  },
+  {
+    "revision": "71f00b92a86f84c061dad44918f4d0af",
+    "url": "/Taraskiiin/react-cursor-1hw.git/static/media/1hbVBwPsnlY.71f00b92.jpg"
+  }
+]);
